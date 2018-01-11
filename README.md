d2f3fa96a1735eff900c16899f594122ca2fb4c7
Kishan <kishan@creolestudios.com>
2018-01-11 15:11:20 +0530

    added doc for MultiComparator

--- README.md
+++ README.md
@@ -2,5 +2,10 @@
 Android's basic key points while developing apps.
 
 #### ListView
-* Dont include ListView in ScrollView
-* ListView must be match_parent
+* Dont include ListView in ScrollView.
+* ListView must be match_parent.
+
+#### List
+* Use [MultiComparator](./code/MultiComparator.java) in [Collections.sort()](https://docs.oracle.com/javase/7/docs/api/java/util/Collections.html#sort(java.util.List,%20java.util.Comparator)) for multiple comparision.
+
+
---
+++ code/MultiComparator.java
@@ -0,0 +1,34 @@
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.Comparator;
+import java.util.List;
+
+/**
+ * Created by kishan on 21/11/17.
+ */
+
+public class MultiComparator<T> implements Comparator<T> {
+    private final List<Comparator<? super T>> comparators;
+
+    public MultiComparator(List<Comparator<? super T>> comparators) {
+        this.comparators = comparators;
+    }
+
+    public MultiComparator(Comparator<? super T>... comparators) {
+        this(Arrays.asList(comparators));
+    }
+
+    public int compare(T o1, T o2) {
+        for (Comparator<? super T> c : comparators) {
+            int result = c.compare(o1, o2);
+            if (result != 0) {
+                return result;
+            }
+        }
+        return 0;
+    }
+
+    public static <T> void sort(List<T> list, Comparator<? super T>... comparators) {
+        Collections.sort(list, new MultiComparator<T>(comparators));
+    }
+}
